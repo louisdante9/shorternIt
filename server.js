5694d64e75e76e96edb774b4289bbcf98c0fd261
Louis Nwamadi <louisdante9@gmail.com>
2020-05-19 22:51:10 +0100

    feat(setup): setup url shortener service with ejs templating engine as view

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models')
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ longUrl: req.body.fullUrl })
+
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ shortUrl: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.longUrl)
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Service is running on ${PORT}`);
+})
